8e233e9bbfb760e61180b00871ab600d25beb4da
motianyi <37366982+motianyi@users.noreply.github.com>
2019-09-15 18:56:44 +1000

    add rmarkdowm

---
+++ Ass2/MAST30027assign2.R
@@ -0,0 +1,8 @@
+dataset = read.delim("assign2.txt", header = TRUE, sep = " ")
+data1 = dataset[dataset$actor == 1,]
+data2 = dataset[dataset$actor == 2,]
+data3 = dataset[dataset$actor == 3,]
+data4 = dataset[dataset$actor == 4,]
+data5 = dataset[dataset$actor == 5,]
+data6 = dataset[dataset$actor == 6,]
+data7 = dataset[dataset$actor == 7,]
