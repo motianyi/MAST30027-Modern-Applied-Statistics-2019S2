0da37381823f2765f69f4bdf7bbf6d44da884b90
motianyi <37366982+motianyi@users.noreply.github.com>
2019-10-24 20:36:01 +1100

    ass4 update

--- Ass3/Ass3.R
+++ Ass3/Ass3.R
@@ -9,14 +9,14 @@ k_hat = mean(quine[,'Days'])
 p_hat_mle = k_hat/(r+k_hat) 
 
 
-simulation<- function(alpha,c,d) {
+simulation<- function(c,d) {
   x = rnorm(1)
-  while (1==1) {
-    if(x > -(1/c)){
-      y = runif(1,0,dnorm(x))
-      if(y < f(x,c,d)) return(x)
-    }
+  y = runif(1,0,dnorm(x))
+  while (x < -(1/c)||y > f(x,c,d)) {
+    x = rnorm(1)
+    y = runif(1,0,dnorm(x))
   }
+  return(x)
 }
 
 f <- function(x,c,d){
@@ -27,30 +27,25 @@ g <- function(x,c,d){
   return(d*log((1+c*x)^3) - d*(1+c*x)^3+d)
 }
 
-alpha = 1.2
-lambda = 3
-d = alpha -1/3
-c = 1/sqrt(9*d)
 
 
 mygamma = function(n,alpha,lambda){
+  d = alpha -1/3
+  c = 1/sqrt(9*d)
+  
   result = rep(0,n)
   for(i in 1:n) {
-    result[i] = ((d * (1 + c*simulation(alpha,c,d))^3) / lambda)
+    result[i] = ((d * (1 + c*simulation(c,d))^3) / lambda)
   }
   return(result)
 }
 
 
-sim = mygamma(1000, 1.2, 3)
+
+
+
+set.seed(2)
+sim = mygamma(1000,1.2,3)
 plot(qgamma(1:1000/1001,1.2,3),sort(sim))
 abline(0,1,col="red")
 
-
-
-
-result = rep(0,1000)
-for(i in 1:1000) {
-  result[i] = ((d * (1 + c*simulation(alpha,c,d))^3) / lambda)
-}
-result
